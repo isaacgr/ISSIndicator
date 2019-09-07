06192be3b27a1df5c24d63c694d47a982396f525
isaacgr <isaacgrowell@gmail.com>
2019-09-07 10:44:46 -0400

    Clean up directory. Fix issues with code.

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import time
 import machine
 import network
 
-SSID = ''
-PASSWORD = ''
+SSID = 'VIRGIN559'
+PASSWORD = '412C7934'
 
 
 class ISSIndicator(object):
@@ -33,9 +33,10 @@ class ISSIndicator(object):
         self.led.on()
 
     def initialize(self):
-        self.led = machine.Pin(0, Pin.OUT, value=0)  # Indicate wifi connection
+        # Indicate wifi connection
+        self.led = machine.Pin(4, machine.Pin.OUT, value=0)
         self.servo_pin = machine.Pin(12)
-        self.servo = machine.PWM(self.servo_pin, freq=50, duty=77)
+        self.servo = machine.PWM(self.servo_pin, freq=50, duty=50)
 
     def get_coordinates(self):
         r = requests.get(self.URL)
@@ -51,30 +52,29 @@ class ISSIndicator(object):
         lat_range = range(int(round(self.LAT))-10, int(round(self.LAT))+10)
         long_range = range(int(round(self.LONG))-10, int(round(self.LONG))+10)
         if int(iss_lat) in lat_range and int(iss_long) in long_range:
-            print 'ISS is overhead!'
+            print('ISS is overhead!')
             self.move_servo(True)
         else:
-            print 'ISS is not overhead. [%s %s]' % (iss_lat, iss_long)
+            print('ISS is not overhead. [%s %s]' % (iss_lat, iss_long))
             self.move_servo(False)
 
     def move_servo(self, status):
         if status == True:
-            self.servo.duty(102)
+            self.servo.duty(98)
         else:
-            self.servo.duty(77)
+            self.servo.duty(50)
 
 
 def main():
     iss = ISSIndicator()
-    iss.connect()
     iss.initialize()
+    iss.connect()
     while True:
         try:
             iss_lat, iss_long = iss.get_coordinates()
             iss.check_location(round(iss_lat), round(iss_long))
         except Exception as e:
-            print 'Error: %s' % e
+            print('Error: %s' % e)
 
 
-if __name__ == '__main__':
-    main()
+main()
